85b05ab1ae486d8227403c20c1d935a4a2c3202f
Marthmaci <mmcichos@gmail.com>
2016-12-10 13:05:08 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -15,7 +15,7 @@ http.createServer(function (request, response) {
         // Send the response body as "Hello Cloud"
         response.end('Hello Cloud\n'); 
    } 
-}).listen(8081);
+}).listen(process.env.PORT);
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
