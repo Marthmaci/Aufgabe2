825acbf381f91003c2b3ea28e44df9f11c0a4516
Marthmaci <mmcichos@gmail.com>
2016-12-10 13:00:25 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,25 +1,21 @@
 var http = require('http');
 
-
-
 http.createServer(function (request, response) {
-
    // Send the HTTP header 
-
    // HTTP Status: 200 : OK
-
    // Content Type: text/plain
-
    response.writeHead(200, {'Content-Type': 'text/plain'});
-
-
-
-    // Send the response body as "Hello Cloud"
-
-    var processId = process.pid;
-
-    response.end('process id: '+processId); 
-
    
+   if(request.method=='POST') {
+       // Send the response body as "Hello Cloud"
+        response.end('Wow, this was an awesome POST request!\n'); 
+   }
+   else
+   {
+        // Send the response body as "Hello Cloud"
+        response.end('Hello Cloud\n'); 
+   } 
+}).listen(8081);
 
-}).listen(process.env.PORT);
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
